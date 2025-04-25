a4be1bf0e93d6659fd86c02771316f89185b80c1
Christoph Welcz <christoph.welcz@codecentric.de>
2025-04-25 13:53:31 +0200

    first experiments with resilience

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,32 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
+  kotlin("jvm") version "2.1.20"
 }
 
 group = "de.welcz"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+  val kotestVersion = "5.9.1"
+  val arrowVersion = "2.1.0"
+
+  implementation("io.arrow-kt:arrow-core:$arrowVersion")
+  implementation("io.arrow-kt:arrow-resilience:$arrowVersion")
+  implementation("io.github.oshai:kotlin-logging:7.0.7")
+  implementation("ch.qos.logback:logback-classic:1.5.18")
+
+  testImplementation(kotlin("test"))
+  testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+  testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+  testImplementation("io.kotest.extensions:kotest-assertions-arrow:2.0.0")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(21)
+  jvmToolchain(21)
 }
--- src/test/kotlin/ResilienceTest.kt
+++ src/test/kotlin/ResilienceTest.kt
@@ -1,2 +1,82 @@
-class ResilienceTest {
+import arrow.core.raise.Raise
+import arrow.core.raise.either
+import arrow.resilience.Schedule
+import arrow.resilience.retryOrElseEither
+import arrow.resilience.retryRaise
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.kotest.assertions.arrow.core.shouldBeLeft
+import io.kotest.assertions.arrow.core.shouldBeRight
+import io.kotest.core.spec.style.DescribeSpec
+import kotlin.time.Duration.Companion.seconds
+
+class ResilienceTest : DescribeSpec({
+  describe("Resiliency test") {
+    val policy = Schedule
+      .forever<BaseError>()
+      .log { error, attempts -> logger.warn { "$error on $attempts attempt" } }
+
+    it("works sometimes :-p") {
+      val result = either {
+        mightFail()
+      }
+
+      result shouldBeRight 42
+    }
+
+    it("works always") {
+      val result = policy.retryRaise { mightFail() }
+
+      result shouldBeRight 42
+    }
+
+    it("works only on specific errors") {
+      val conditionalPolicy = policy.doWhile { error, _ -> error is WtfError }
+
+      val result = conditionalPolicy.retryRaise { mightFail() }
+
+      result shouldBeRight 42
+    }
+
+    it("works mostly with max retries, jitter and backoff") {
+      val realWorldPolicy =
+        (Schedule.recurs<BaseError>(5) and Schedule.exponential(1.seconds)).jittered()
+          .log { error, output -> logger.warn { "$error with $output" } }
+
+      val result = realWorldPolicy.retryRaise { mightFail() }
+
+      result shouldBeRight 42
+    }
+
+    it("works for classical exceptions") {
+      val exceptionPolicy = Schedule.recurs<Throwable>(5)
+        .log { error, attempts -> logger.warn { "$error, attempt $attempts" } }
+
+      val result = exceptionPolicy
+        .retryOrElseEither(
+          {
+            throw RuntimeException("BAM!")
+          },
+          { err, output -> WtfError }
+        )
+
+      result shouldBeLeft WtfError
+    }
+  }
+})
+
+private fun Raise<BaseError>.mightFail(): Int {
+  val random = (1..3).random()
+  if (random == 1) {
+    raise(WtfError)
+  }
+  if (random == 2) {
+    raise(ThisIsFineError)
+  }
+  return 42
 }
+
+sealed class BaseError(val message: String)
+data object WtfError : BaseError("WTF???")
+data object ThisIsFineError : BaseError("This is a Fine")
+
+val logger = KotlinLogging.logger {}
